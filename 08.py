fd4307e469dfde6cf56806fa121514e63146e61a
Shivam12-ui558 <shivamkumar12082004@gmail.com>
2024-09-26 20:13:58 +0530

    PYTHON FILE

---
+++ 08.py
@@ -0,0 +1,17 @@
+class college:
+    def __init__(self,n,loc):
+        self.name=n
+        self.location=loc
+    def info(self):
+        print(self.name+"is located in "+self.location)
+class Department(college):
+    def __init__(self,ids,n):
+        self.id=ids
+        self.name=n
+        super ().__init__(n,"Gec Vaishali")
+    def info_d(self):
+        print(self.id+" is department code of "+self.name)
+
+D=Department("105","CSE ")
+D.info_d()
+D.info()
